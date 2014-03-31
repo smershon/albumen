1a57e2abee4e15673b38700475e2588e4ff2f80e
smershon <stuart@echonest.com>
2014-03-31 09:51:05 -0400

    some bug fixes, make a desktop

--- squarepack.py
+++ squarepack.py
@@ -1,4 +1,5 @@
 import fractions
+import math
 import random
 import sys
 import os
@@ -6,6 +7,8 @@ import os
 import Image
 
 from pprint import pprint
+
+import analysis_cache
 
 def random_iterate(src):
     new_src = list(src)
@@ -49,10 +52,8 @@ class ImageGrid(object):
         return candidates
 
     def _can_join(self, i, j):
-        print '(%d, %d)' % (i,j)
         for coords in ((i,j), (i+1,j), (i,j+1), (i+1,j+1)):
             if coords not in self.data:
-                print 'not in data'
                 return False
             if self.data[coords][0] != 1:
                 return False
@@ -60,9 +61,9 @@ class ImageGrid(object):
 
 def gen_grid(x, y, n):
     """
-    x - max number of cells horizontally
-    y - max number of cells vertically
-    n - number of images to fit (if n >= x*y, return x*y)
+        x - max number of cells horizontally
+        y - max number of cells vertically
+        n - number of images to fit (if n >= x*y, return x*y)
     """
     g = ImageGrid(x, y)
     if not grid_reduce(g, n):
@@ -83,34 +84,36 @@ def grid_reduce(g, n):
             g.unjoin(*coord)
     return False
 
-def gen_image(g, d, cell=100):
+def gen_image(g, attr, cell=100):
     img = Image.new('RGBA', (cell*g.x, cell*g.y))
-    sources = [os.path.join(d, x) for x in os.listdir(d)]
-    print len(sources)
+    sources = [x.meta['filename'] for x in analysis_cache.get_images(sort_field=attr)]
     for coord, spec in g.data.iteritems():
-        src = sources.pop()
+        src = sources.pop(0)
         print coord, spec[0], src
-        src_img = Image.open(src).resize((cell*spec[0], cell*spec[0]))
+        src_img = Image.open('samples/%s' % src).resize((cell*spec[0], cell*spec[0]))
         box = (cell*coord[0], cell*coord[1], cell*(coord[0] + spec[0]), cell*(coord[1] + spec[0]))
         img.paste(src_img, box)
     return img
 
 def gen_spec(xpx, ypx, n):
     """
-    in: xpx, ypx, n
-    out: xcells, ycells, cellsize
+        in: xpx, ypx, n
+        out: xcells, ycells, cellsize
     """ 
     gcd = fractions.gcd(xpx, ypx)
     min_xcells = xpx/gcd
     min_ycells = ypx/gcd
-    scale = n/(min_xcells*min_ycells) + 1
+    scale = int(math.ceil(float(n)/(min_xcells*min_ycells)))
     return scale*min_xcells, scale*min_ycells, xpx/(scale*min_xcells)
 
-def main(x,y,n):
+def main(x,y,n, attr):
     xcell, ycell, cellsize = gen_spec(x, y, n)
+    print [xcell, ycell, cellsize, x, y, n]
+    exit()
     g = gen_grid(xcell, ycell, n)
-    im = gen_image(g, 'samples', cell=cellsize)
+    im = gen_image(g, attr=attr, cell=cellsize)
+    im.save('bg_%s.png' % attr, format='PNG')
     im.show()
 
 if __name__ == '__main__':
-    main(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+    main(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), sys.argv[4])
